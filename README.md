1d39d2b8c1cc1d696916dce71f9fa983cb7b8ceb
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-01-27 14:17:53 -0500

    new module

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # module_header_tags_gpublisher
+
 Metatag provide information about a given Web page, most often to help search engines categorize them correctly. They are inserted into the HTML document, but more often are not directly visible to a user visiting the site.
+
+They have been a field of marketing research known as search engine optimization (Search Engine Optimization, SEO) name, in which we explore different methods to provide the website a user a better rank in search engines
+
+Google Publisher is a new tag concerning the advertisement
+
+This module contains
+
+- The language files in English and French
+  
+Via the installation system administration ClicShopping
+
+Technical Prerequisites: None
+
+Modules:
+
+- Compatibility: >= version 3.0
+
+Recommendation and documentation specific use :
+
+
+Implementation:
+
+- Install the module
+- Activate the module
+
+All informations about the ClicShopping
+
+ Download ClicShopping : https://github.com/ClicShopping/ClicShopping_V3/archive/master.zip
+
+ Community : https://www.clicshopping.org
+
+ Software : https://github.com/ClicShopping
+
+ Official add on : https://github.com/ClicShoppingOfficialModulesV3
+
+ Community add on : https://github.com/ClicShoppingV3Community
+
+ trademark License info : https://www.clicshopping.org/forum/trademark/ 
---
+++ includes/modules/header_tags/ht_gpublisher.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+  use ClicShopping\OM\HTML;
+  use ClicShopping\OM\Registry;
+  use ClicShopping\OM\CLICSHOPPING;
+
+  class ht_gpublisher {
+    public $code;
+    public $group;
+    public $title;
+    public $description;
+    public $sort_order;
+    public $enabled = false;
+
+    public function __construct() {
+      $this->code = get_class($this);
+      $this->group = basename(__DIR__);
+      $this->title = CLICSHOPPING::getDef('module_header_tags_gpublisher_title');
+      $this->description = CLICSHOPPING::getDef('module_header_tags_gpublisher_description');
+
+      if ( defined('MODULE_HEADER_TAGS_GPUBLISHER_STATUS') ) {
+        $this->sort_order = MODULE_HEADER_TAGS_GPUBLISHER_SORT_ORDER;
+        $this->enabled = (MODULE_HEADER_TAGS_GPUBLISHER_STATUS == 'True');
+      }
+    }
+
+    public function execute() {
+
+      $CLICSHOPPING_Template = Registry::get('Template');
+
+      $CLICSHOPPING_Template->addBlock('<link rel="publisher" href="' . HTML::output(MODULE_HEADER_TAGS_GPUBLISHER_ID) . '" />' . "\n", $this->group);
+    }
+
+    public function isEnabled() {
+      return $this->enabled;
+    }
+
+    public function check() {
+      return defined('MODULE_HEADER_TAGS_GPUBLISHER_STATUS');
+    }
+
+    public function install() {
+      $CLICSHOPPING_Db = Registry::get('Db');
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Souhaitez vous activer ce module ?',
+          'configuration_key' => 'MODULE_HEADER_TAGS_GPUBLISHER_STATUS',
+          'configuration_value' => 'True',
+          'configuration_description' => 'Souhaitez vous activer ce module ?',
+          'configuration_group_id' => '6',
+          'sort_order' => '1',
+          'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Lien de Google+ Publisher',
+          'configuration_key' => 'MODULE_HEADER_TAGS_GPUBLISHER_ID',
+          'configuration_value' => '',
+          'configuration_description' => 'Votre URL Google +',
+          'configuration_group_id' => '6',
+          'sort_order' => '1',
+          'set_function' => '',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Ordre de tri d\'affichage',
+          'configuration_key' => 'MODULE_HEADER_TAGS_GPUBLISHER_SORT_ORDER',
+          'configuration_value' => '115',
+          'configuration_description' => 'Ordre de tri pour l\'affichage (Le plus petit nombre est montré en premier)',
+          'configuration_group_id' => '6',
+          'sort_order' => '95',
+          'set_function' => '',
+          'date_added' => 'now()'
+        ]
+      );
+
+       return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
+                                               ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
+                            );
+    }
+
+    public function remove() {
+      return Registry::get('Db')->exec('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
+    }
+
+    public function keys() {
+      return array('MODULE_HEADER_TAGS_GPUBLISHER_STATUS',
+                   'MODULE_HEADER_TAGS_GPUBLISHER_ID',
+                   'MODULE_HEADER_TAGS_GPUBLISHER_SORT_ORDER'
+                   );
+    }
+  }
+
